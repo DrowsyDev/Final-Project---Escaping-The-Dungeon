892f528ea7db1091ad4b053f2c4c1e29de103f23
Roberto Santoyo <>
2021-05-27 18:19:40 +0000

    Added JOptionPane to get user input in the form of asking for a name.

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ class Main
 {
   public static void main(String[] args) 
   {
+    //Player input
     String playerName = JOptionPane.showInputDialog("What's your name?");
 
     JOptionPane.showMessageDialog(null, "Ah what a curious name. Hello " + playerName);
